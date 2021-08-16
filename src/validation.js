381fd5a8234eff314a4909b1901c2075be54771d
pavan kumar <pavan.yalamanchi@gmail.com>
2021-08-16 14:00:02 +0100

    merged to develop

--- src/validation.js
+++ src/validation.js
@@ -1,6 +1,8 @@
 import { body } from "express-validator";
 
-export const bodyValidation = [
-  body("comment").exists().withMessage("Comment is a mandatory field"),
-  body("rate").exists().withMessage("Rate is a mandatory field"),
+const bodyValidation = [
+    body("comment").exists().withMessage("Comment is a mandatory field"),
+    body("rate").exists().withMessage("Rate is a mandatory field"),
 ];
+
+export default bodyValidation
